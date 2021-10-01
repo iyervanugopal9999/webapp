5d09a4003eaf724bd92127eaed2d1d3a493ae245
Arnab2010 <72144787+Arnab2010@users.noreply.github.com>
2021-10-01 08:53:57 +0530

    complexity and space improvised

--- test/run.sh
+++ test/run.sh
@@ -8,6 +8,30 @@ ret=$?
 if [ $ret -ne 0 ]; then
 exit $ret
 fi
+rm -rf targetret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf targetret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf targetret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf targetret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf targetret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf targetret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
 rm -rf target
 
 ./gradlew build
@@ -17,9 +41,15 @@ exit $ret
 fi
 rm -rf build
 
-cd ../initial
-
-./mvnw clean compile
+cd ../initialret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf targetret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
 ret=$?
 if [ $ret -ne 0 ]; then
 exit $ret
